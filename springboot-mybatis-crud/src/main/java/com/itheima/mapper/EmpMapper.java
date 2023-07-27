4ed5ed0f60fc7f2b5db2edf108c695fad8116fa8
zhaohangyi <1806472041@qq.com>
2023-07-27 15:00:10 +0800

    上传测试

---
+++ springboot-mybatis-crud/src/main/java/com/itheima/mapper/EmpMapper.java
@@ -0,0 +1,11 @@
+package com.itheima.mapper;
+
+import org.apache.ibatis.annotations.*;
+
+@Mapper
+public interface EmpMapper {
+
+    //根据 id 删除数据
+    @Delete("delete from emp where id = #{id}")
+    public void delete(Integer id);
+}
