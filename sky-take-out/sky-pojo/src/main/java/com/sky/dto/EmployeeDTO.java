ae8628432ac8dcb46dcf4ce80941e5411da41401
zhaohangyi <1806472041@qq.com>
2023-08-31 16:35:04 +0800

    新增员工业务代码开发

--- sky-take-out/sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-take-out/sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -1,22 +1,30 @@
 package com.sky.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@ApiModel("新增员工的dto")
 public class EmployeeDTO implements Serializable {
 
     private Long id;
 
+    @ApiModelProperty("用户名")
     private String username;
 
+    @ApiModelProperty("姓名")
     private String name;
 
+    @ApiModelProperty("电话")
     private String phone;
 
+    @ApiModelProperty("性别")
     private String sex;
 
+    @ApiModelProperty("身份证")
     private String idNumber;
 
 }
